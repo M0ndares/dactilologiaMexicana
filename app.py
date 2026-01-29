3c6fe4db412c2814b6c0af8f7fe97f4fe3b9ea5a
Osval Montesinos <omontesinos@ucol.mx>
2026-01-29 08:23:04 -0600

    DactiMex V.3

--- app.py
+++ app.py
@@ -121,5 +121,5 @@ def predict():
         return jsonify({'error': f"Error: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 10001))
     app.run(host='0.0.0.0', port=port)
