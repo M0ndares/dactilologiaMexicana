847adf24409e1c219bf9ab50fdc71a88eff22435
Osval Montesinos <omontesinos@ucol.mx>
2026-04-05 17:48:17 -0600

    preprocess

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def prepare_image(file_stream):
         x = np.expand_dims(x, axis=0)
         
         del img, img_rgb, file_bytes
-        return (x / 255.0) 
+        return (x / 127.5) - 1.0
     
     return None
 
