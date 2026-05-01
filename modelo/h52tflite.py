8b3e9540bb1dea9ded84783edb8a5b76daf785ca
Osval Montesinos <omontesinos@ucol.mx>
2026-05-01 12:59:22 -0600

    portada

--- modelo/h52tflite.py
+++ modelo/h52tflite.py
@@ -1,6 +1,6 @@
 import tensorflow as tf 
-model = tf.keras.models.load_model('model2.h5')
+model = tf.keras.models.load_model('model3.h5')
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 modelLite = converter.convert()
-with open('model2.tflite', 'wb') as f:
+with open('model3.tflite', 'wb') as f:
     f.write(modelLite)
